a1f37ca2c6ce9ff9213ce23c3371e6a99c4b5af3
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-05-06 14:45:54 +0700

    2

---
+++ PointerdanFriendClass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+    public : // akses modifier
+        int panjang, lebar;
+
+    public : 
+
+        void input()
+        {
+            
+        }
+}
