fc30457c9c3774e9ce5760cbcfb44ecc278b445f
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-05-06 15:17:21 +0700

    10

--- PointerdanFriendClass.cpp
+++ PointerdanFriendClass.cpp
@@ -50,5 +50,5 @@ int main()
     cin >> objekPP.panjang;
     cout << "Masukkan Lebar : ";
     cin >> objekPP.lebar;
-    cout << "Luas Persegi Panjang :" << 
+    cout << "Luas Persegi Panjang :" << objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
 }
